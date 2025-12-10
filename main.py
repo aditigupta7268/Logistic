358fde6d510ec1ab02f9397cd045cf4e286b5c07
aditi7268 <99127320+aditigupta7268@users.noreply.github.com>
2025-12-10 18:30:36 +0530

    Migrate from Flask to FastAPI framework

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from flask import Flask
-import os
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
-# Your routes here
-@app.route('/')
-def home():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
