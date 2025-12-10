167dfac832c19c085751be1a166acf741ba973ad
aditi7268 <99127320+aditigupta7268@users.noreply.github.com>
2025-12-10 18:24:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from flask import Flask
 import os
-port = int(os.environ.get("PORT", 10000))
-app.run(host='0.0.0.0', port=port)
+
+app = Flask(__name__)
+
+# Your routes here
+@app.route('/')
+def home():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
